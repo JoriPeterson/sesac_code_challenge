32030c0cba5955158cf200232a3b0ab804f08090
Jori Peterson <joripeterson@gmail.com>
2019-12-11 21:01:26 -0700

    Test it can locate princess

--- test/princess_path_test.rb
+++ test/princess_path_test.rb
@@ -6,6 +6,7 @@ class PrincessPathTest < Minitest::Test
 
   def setup
     @path = PrincessPath.new
+    @grid = @path.create_grid(3)
   end
 
   def test_it_exists
@@ -21,9 +22,8 @@ class PrincessPathTest < Minitest::Test
   end
 
   def test_it_places_princess_in_a_corner
-    grid = @path.create_grid(3)
-    @path.place_princess(grid, 1)
-    assert_equal "p", grid[0][0]
+    @path.place_princess(@grid, 1)
+    assert_equal "p", @grid[0][0]
 
     grid_2 = @path.create_grid(5)
     @path.place_princess(grid_2, 3)
@@ -31,11 +31,15 @@ class PrincessPathTest < Minitest::Test
   end
 
   def test_it_can_get_middle_index
-    grid = @path.create_grid(3)
-    assert_equal 'm', @path.get_middle_index(grid)
+    assert_equal 'm', @path.get_middle_index(@grid)
 
     grid_2 = @path.create_grid(41)
     assert_equal 'm', @path.get_middle_index(grid_2)
+  end
+
+  def test_it_can_locate_princess
+    @path.place_princess(@grid, 1)
+    assert_equal 1, @path.locate_princess(@grid)
   end
 
 end
