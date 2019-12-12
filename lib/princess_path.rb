9bd767c32d80f827695f466552aa1123209f1514
Jori Peterson <joripeterson@gmail.com>
2019-12-11 19:44:01 -0700

    Create grid and outline next steps

--- lib/princess_path.rb
+++ lib/princess_path.rb
@@ -1,14 +1,33 @@
 class PrincessPath
 
-  def displayPathtoPrincess(n,grid)
+  def create_grid(n)
+    Array.new(n) { Array.new(n,'-') }
+  end
+
+  def get_four_corner_indexes
+    # return indexes of four corners
+    # first array, first & last index
+    # last array, first & last index
+  end
+
+  def place_princess
+    # randomly place princess('p') in one of four corners
+  end
+
+  def get_middle_index
+  end
+
+  def locate_princess
+    # this function looks through the 4 corners
+    # to check for the 'p'
+  end
+
+  def generate_path
+  end
+
+  def displayPathtoPrincess#(n,grid)
     return ""
   end
 
-  n = gets.to_i
-  
-  (0...n).each do |i|
-    grid[i] = gets.strip
-  end
-
-  displayPathtoPrincess(n,grid)
+  #displayPathtoPrincess(n,grid)
 end
