74c96c31fcdae3aa51766a391d4c8009e6e44bab
Jori Peterson <joripeterson@gmail.com>
2019-12-13 10:45:56 -0700

    Add testing for higher coverage

--- test/princess_path_test.rb
+++ test/princess_path_test.rb
@@ -1,6 +1,8 @@
+require_relative 'test_helper'
 require 'minitest/autorun'
 require 'pry'
 require './lib/princess_path'
+
 
 class PrincessPathTest < Minitest::Test
 
@@ -50,11 +52,26 @@ class PrincessPathTest < Minitest::Test
 
   def test_it_generates_path
     @path.place_princess(@grid, 1)
+    assert_equal ["UP", "LEFT"], @path.generate_path(@grid)
+  end
+
+  def test_it_generates_path_2
+    @path.place_princess(@grid, 2)
     assert_equal ["UP", "RIGHT"], @path.generate_path(@grid)
   end
 
+  def test_it_generates_path_3
+    @path.place_princess(@grid, 3)
+    assert_equal ["DOWN", "LEFT"], @path.generate_path(@grid)
+  end
+
+  def test_it_generates_path_4
+    @path.place_princess(@grid, 4)
+    assert_equal ["DOWN", "RIGHT"], @path.generate_path(@grid)
+  end
+
   def test_is_displays_path_to_princess
-    @path.place_princess(@grid, 1)
+    @path.place_princess(@grid, 2)
     assert_equal "UP\nRIGHT", @path.displayPathtoPrincess(3, @grid)
   end
 end
