0449d25b7995c71936fd19038aacff62afb7be5b
Jori Peterson <joripeterson@gmail.com>
2019-12-13 11:35:18 -0700

    Test it places characters on grid

--- test/random_princess_path_test.rb
+++ test/random_princess_path_test.rb
@@ -1,25 +1,31 @@
 require_relative 'test_helper'
 require 'minitest/autorun'
 require 'pry'
+require './lib/princess_path'
 require './lib/random_princess_path'
 
 class RandomPrincessPathTest < Minitest::Test
 
   def setup
-    @path = RandomPrincessPath.new
+    @path = PrincessPath.new
     @grid = @path.create_grid(3)
+    @random_path = RandomPrincessPath.new
   end
 
   def test_it_randomly_places_princess
-    @path.place_princess(@grid)
+    @random_path.place_princess(@grid)
     assert true, @grid.include?('p')
   end
 
   def test_it_randomly_places_mario
-    skip
-    @path.place_princess(@grid)
-    @path.place_mario(@grid)
+    @random_path.place_princess(@grid)
+    @random_path.place_mario(@grid)
     assert true, @grid.include?('p')
     assert true, @grid.include?('m')
+  end
+
+  def test_it_finds_character_on_grid
+    @random_path.place_princess(@grid)
+    @random_path.place_mario(@grid)
   end
 end
